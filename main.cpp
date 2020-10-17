4b21b34a459e84c3c365064b1da719f2a8177a3d
unknown <javierjose1224>
2020-10-17 16:08:59 -0500

    Declaracion de variables

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int op,Ho,Vo,Hd,Vd,alpo,alpd,dist;
+    bool ver=true;
+
+    while (ver)
+        cout<<"1 para disparos ofensivos aciertan defensivo: ";
+        cout<<"2 disparos de defensivo aciertan ofensivo  ";
+        cout<<"3 para disparos defensivos: ";
+        cout<<"4 para disparos : ";
+        cin>>op;
+    //generar distapros ofensivos
+        switch(op)
+        {
+            case 1:
+            {
+                cout<<"distancia entre los cañones: (d) ";
+            }
+            break;
+
+            case 2:
+            {
+
+            }
+
+            break;
+
+            case 3:
+            {
+
+            }
+            break;
+
+            case 4:
+            {
+
+            }
+            break;
+
+        }
+
+
+    return 0;
+}
